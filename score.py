0e144a9feee75fcd2468c2af1777c67c83feba78
Rony Reis <29354456+reis-r@users.noreply.github.com>
2021-01-30 13:37:15 -0300

    refactor: use f-strings

--- score.py
+++ score.py
@@ -6,14 +6,15 @@ import joblib
 import sklearn
 def init():
     global model
-    model_path = Model.get_model_path('glass-prediction') # Renamed the model
-    print("Model path: " + model_path)
+    model_path = Model.get_model_path('glass-prediction')
+    print(f"Model path: {model_path}")
     model = joblib.load(model_path)
 def run(data):
     try:
         data = np.array(json.loads(data))
+        print(f"Data received: {data}")
         result = model.predict(data)
-        # You can return any data type, as long as it is JSON serializable.
+        print(f"result: {result}")
         return result.tolist()
     except Exception as e:
         error = str(e)
